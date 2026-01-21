61b1994ccc44ffdbcf164b8ab3f2f1c7dbdb58fd
Nicolas Andres Zarate <nicolasandreszarate@gmail.com>
2026-01-20 23:38:39 -0500

    Amor te amo

--- amor.js
+++ amor.js
@@ -5,5 +5,6 @@ console.log("TE AMO MUCHO SARA GABIIIIII");
 console.log("AMOR, TE AMO");
 console.log("Hoy estas un poquito mejor conmigo");
 console.log("Que lindo dia tuvimos hoy gabi, descansa");
+console.log("Hay un hotel que reservo gabi");
 
 
